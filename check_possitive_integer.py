09d6f29d0497bd7a2565ad0f279345428f6b0deb
Alfred Wong <wyfalfred@gmail.com>
2022-06-14 22:17:29 -0500

    first commit

---
+++ check_possitive_integer.py
@@ -0,0 +1,12 @@
+number = input('Please input a possitive integer')
+
+if (number.isnumeric()):
+	number = int(number)
+	if number > 20:
+		print('your input: ' + str(number) + ' > 20')
+	if number > 10:
+		print('your input: ' + str(number) + ' > 10')
+	if number > 0:
+		print('your input: ' + str(number) + ' > 0')
+else:
+	print('your input is not an integer or it is negative')
